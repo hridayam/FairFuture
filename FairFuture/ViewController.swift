b2977d6d046d18b0eb5900fb77fe0b843908fceb
hridayam <hridayambakshi2011@gmail.com>
2018-03-01 23:22:27 -0800

    cloudkit integration and demo code

--- FairFuture/ViewController.swift
+++ FairFuture/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uploadedNumbers: UILabel!
+    
+    var randomNumbers: [Int] = []
+    
+    func loadData() {
+        let publicData = CKContainer.default().publicCloudDatabase
+        let query = CKQuery(recordType: "numbers", predicate: NSPredicate(format: "TRUEPREDICATE", argumentArray: nil))
+        publicData.perform(query, inZoneWith: nil, completionHandler: { (results: [CKRecord]?, error: Error?) -> Void in
+            if let records = results {
+                for i in 0..<records.count {
+                    self.randomNumbers.append(Int((records[i]["content"] as? String)!)!)
+                }
+            } else {
+                print(error?.localizedDescription)
+            }
+            DispatchQueue.main.async {
+                self.setLabel()
+            }
+        })
+    }
+    
+    func setLabel () {
+        var temp: String = ""
+        for i in 0..<randomNumbers.count {
+            if temp == "" {
+                temp = "\(randomNumbers[i])"
+            } else {
+                temp = "\(temp)\n\(randomNumbers[i])"
+            }
+        }
+        
+        uploadedNumbers.numberOfLines = randomNumbers.count
+        uploadedNumbers.text = temp
+        randomNumbers.removeAll()
+    }
+    
+    @IBAction func uploadRandNumbers(_ sender: Any) {
+        let newRecord = CKRecord(recordType: "numbers")
+        newRecord["content"] = String(Int(arc4random_uniform(UInt32(100)))) as NSString
+        let publicData = CKContainer.default().publicCloudDatabase
+        publicData.save(newRecord, completionHandler: { (record: CKRecord?, error:Error?) -> Void in
+            if error == nil {
+                print ("record saved")
+                self.loadData()
+            } else {
+                print(error?.localizedDescription)
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
