cf8b5676100559300629732ea868fa202c49e586
karanova99 <karansheth459@gmail.com>
2018-04-24 22:51:04 -0700

    new UI merged

---
+++ client/FairFuture/ViewControllers/Connections/ConnectionsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ConnectionsViewController.swift
+//  FairFuture
+//
+//  Created by admin on 4/23/18.
+//  Copyright © 2018 hridayam bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionsViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageConnections: UIImageView!
+    @IBOutlet weak var profileNameConnections: UILabel!
+    @IBOutlet weak var profileEmailConnection: UILabel!
+    
+    var sentData:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = sentData
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
